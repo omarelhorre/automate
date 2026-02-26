4d115df1190af25c6f01531c225b35e234ecd166
omarelhorre <elhorre.omar@etu.uae.ac.ma>
2026-02-26 21:39:14 +0000

    Added dot files and png for visualization

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Partie 1 : Lecture et affichage d'un automate 
+// Une structure de données qui peut etre utilisé est un graphe orienté evalué les sommets representent les états et les arc représentent l'alphabet en cours de lecture
+int main()
+{
+
+
+	return 0;
+}
