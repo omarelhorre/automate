303f5782e5e71f5379779e8ccab49a5b51c8a8f5
omarelhorre <elhorre.omar@etu.uae.ac.ma>
2026-02-26 22:22:15 +0000

    Added more function utilities

--- header.c
+++ header.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define A[20][20]
 void readDot(void)
 	{
 	FILE* f;
@@ -9,5 +9,6 @@ void readDot(void)
 		{
 			printf("File not opened, please check if your graph.dot file exists in this 					directory");
 		}
+	fscanf(file, "
+	while( fscanf(f," %c -> %c [label=\"%d\"]",&src,&dest,&val)
 	}
-
