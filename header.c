155dfec0f3b7d3e577b63b9fdbfc510579726f64
omarelhorre <elhorre.omar@etu.uae.ac.ma>
2026-02-26 23:24:06 +0000

    Added fillMatrices() and initMatrices() as well as valuations matrices for labelizing in graphviZ

--- header.c
+++ header.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 20
-void readDot(int A[20][20])
+int A[MAX][MAX];
+char val[MAX][MAX];
+void initMatrices(int A[MAX][MAX], char val[MAX][MAX])
+	{
+		for(int i = 0; i<20 ; i++)
+		{
+			for(int j = 0 ; j<20 ; j++)
+			{
+				A[i][j] = 0;
+				val[i][j] = '';
+			}
+		}
+	}
+void fillMatrices(int A[MAX][MAX], char val[MAX][MAX])
+	{
+		int u,v;
+		printf("Entrer le nombres d'alphabets dans votre mots");
+		int alphabets;
+		scanf("%d",alphabets);
+		initMatrices(A,val);
+		for(int i =0; i<alphabets ; i++)
+			{
+				scanf("%d,%d",u,v);
+				A[u][v] = 1;
+			}
+	}
+
+void readDot(int A[MAX][MAX], val[MAX][MAX])
 	{
 	FILE* f;
 	fptr = fopen("graph.dot","r");
 	if(fptr == NULL)
 		{
-			printf("File not opened, please check if your graph.dot file exists in this directory");
+			printf("Fichier introuvable, veuillez vous assurez de l'emplacement du fichier.");
 		}
+	initMatrices(A,val);
 	fscanf(f, "%s");
 	while( fscanf(f," %c -> %c [label=\"%d\"]",&src,&dest,&val ) == 3)
 			{
 				A[src][dest] = 1;
+				val[i][j] = label;
 			}
+	}
+void writeDot(int A[MAX][MAX], val[MAX][MAX])
+	{
+	
 	}
