2f57abf946d154769a8bc7e03beb8c98070a40b7
MaryamAnahir <maryamanahir6@gmail.com>
2026-03-05 23:15:46 +0100

    code organization1

---
+++ file.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+typedef struct Transition{
+    char etat_dep;
+    char etat_arriv;
+    char lettre;
+}Transition;
+
+typedef struct Automate{
+    char etats[10];
+	int nbr_etat;
+    Transition transitions[20];
+	int nbr_trans;
+    char Alphabet[8];
+	int nbr_alph;
+    char etat_initiaux[5];
+	int inic;//nbr des etats initial
+    char etat_finaux[5];
+	int finc;//nbr des etats finaux
+}Automate;
+
+bool rechercherEtat(Automate *protocol, char etatChar){
+	for(int i = 0 ; i<= protocol->nbr_etat;i++){
+		if(protocol->etats[i] == etatChar ){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool rechercherAlphabet(Automate *protocol, char etatChar){
+	for(int i = 0 ; i<= protocol->nbr_alph;i++){
+		if(protocol->Alphabet[i] == etatChar ){
+			return true;
+		}
+	}
+	return false;
+}
+
+void readDot(Automate *protocol,char *fichier){
+	int i=0,inic,finc;
+	char src,dest,val,ligne[100], buff[20];
+	protocol->nbr_etat = 0;
+    protocol->nbr_trans = 0;
+    protocol->nbr_alph = 0;
+	protocol->inic=0;
+	protocol->finc=0;
+	FILE* f = fopen(fichier,"r");
+	if(f == NULL){
+		printf("Fichier introuvable, veuillez vous assurez de l'emplacement du fichier.");
+	}
+	while(fgets(ligne, sizeof(ligne), f)){
+	    if(sscanf(ligne," %c -> %c [label=\"%c\"];",&src,&dest,&val) == 3){
+		//stockage des transitions
+			protocol->transitions[i].etat_dep = src;
+			protocol->transitions[i].etat_arriv = dest;
+			protocol->transitions[i].lettre = val;
+			i++;
+		//stockage des etats
+		    if(protocol->nbr_etat < 10){
+			    if(rechercherEtat(protocol, src) == false){
+					protocol->etats[protocol->nbr_etat] = src;
+					protocol->nbr_etat++;
+			    }
+			    else if(rechercherEtat(protocol, dest) == false){
+					protocol->etats[protocol->nbr_etat] = dest;
+					protocol->nbr_etat++;
+			    }
+            }
+		//stockage des alphabets
+		    if(protocol->nbr_alph < 8){
+			    if(rechercherAlphabet(protocol, val) == false){
+					protocol->Alphabet[protocol->nbr_alph] = val;
+					protocol->nbr_alph++;
+			    }
+            }
+	    }
+    //stockage des etats initiaux et finaux
+	    else if(sscanf(ligne," init -> %c;", &src) == 1){
+			protocol->etat_initiaux[protocol->inic] = src;	
+			protocol->inic++;
+	    }
+        else if(sscanf(ligne," %c -> %[^;];", &src,buff) == 2){
+		    if (strcmp(buff,"fin") != 0)
+                continue ;
+		    protocol->etat_finaux[protocol->finc] = src;
+		    protocol->finc++;
+	    }
+	    else continue;
+	}
+	protocol->nbr_trans = i;
+	fclose(f);
+}
+
+void automateShow(Automate protocol){
+	int i;
+	printf("Voici la liste des etats :\n");	
+	for( i = 0; i< protocol.nbr_etat ; i++){
+		printf("[%c] ",protocol.etats[i]);
+	}
+	printf("\nVoici la liste d'alphabet : \n");	
+	for(i = 0; i< protocol.nbr_alph ; i++){
+		printf("[%c] ", protocol.Alphabet[i]);
+	}
+	printf("\nVoici la liste des transitions :\n");	
+	for( i = 0; i< protocol.nbr_trans ; i++){
+		printf("[%c] -> %c -> [%c]\n",protocol.transitions[i].etat_dep,protocol.transitions[i].lettre,protocol.transitions[i].etat_arriv);
+	}
+	printf("\nVoici la liste des etats initiaux :\n");	
+	for( i = 0; i< protocol.inic ; i++){
+		printf("[%c] ",protocol.etat_initiaux[i]);
+	}
+	printf("\nVoici la liste des etats finaux :\n");	
+	for( i = 0; i< protocol.finc ; i++){
+		printf("[%c] ",protocol.etat_finaux[i]);
+	}
+}
+void MaxTransitions(Automate *protocol){
+    int maxSort = 0, maxEntr = 0;
+    char etatMaxSort, etatMaxEntr;
+  int i ,j;
+    for(i= 0; i < protocol->nbr_etat; i++){
+        int nbSorties = 0;
+        int nbEntrees = 0;
+
+        for( j = 0; j < protocol->nbr_trans; j++){
+            if(protocol->transitions[j].etat_dep == protocol->etats[i]){
+                nbSorties++;
+            }
+            if(protocol->transitions[j].etat_arriv == protocol->etats[i]){
+                nbEntrees++;
+            }
+        }
+
+        if(nbSorties > maxSort){
+            maxSort = nbSorties;
+            etatMaxSort = protocol->etats[i];
+        }
+
+        if(nbEntrees > maxEntr){
+            maxEntr = nbEntrees;
+            etatMaxEntr = protocol->etats[i];
+        }
+    }
+
+    printf("Etat avec max transitions sortantes : %c (%d)\n", etatMaxSort, maxSort);
+    printf("Etat avec max transitions entrantes : %c (%d)\n", etatMaxEntr, maxEntr);
+}
+void afficherEtatsAvecTransition(Automate *protocol, char lettre) {
+    bool aTransition = false;
+    printf("Transitions etiquetees par '%c' :\n", lettre);
+
+    for (int i = 0; i < protocol->nbr_etat; i++) {
+
+        
+
+        for (int j = 0; j < protocol->nbr_trans; j++) {
+
+            if (protocol->transitions[j].etat_dep == protocol->etats[i] &&
+                protocol->transitions[j].lettre == lettre) {
+
+                if (!aTransition) {
+                    printf("Etat [%c] -> ", protocol->etats[i]);
+                    aTransition = true;
+                }
+
+                printf("[%c] ", protocol->transitions[j].etat_arriv);
+            }
+        }
+
+        
+    }
+    if (!aTransition) {
+        printf("n'existe pas\n");
+    }
+}
+/*
+void SaveAcceptedWords(Automate *A){
+	char mot[10];
+	FILE *ftxt = fopen("fichier.txt","r"), *fptr = fopen("MotsAccepter.txt","w");
+	if (ftxt == NULL || fptr == NULL){
+		printf("Fichier introuvable, veuillez vous assurez de l'emplacement du fichier.");
+		return;
+	}
+	while( fscanf(ftxt,"%s",mot) == 1){
+		if(estAccepter(&A,mot) == true){
+		 	fprintf(fptr,"%s \n",mot);
+		}
+	}
+}*/
+int menu(void){
+		int choice;
+		printf("-----------AUTOMATE--------\n""1. Lire l'automate depuis graph.dot\n""2. Afficher les informations de l'automate\n""3.Quitter\n""Votre choix: ");
+		scanf("%d",&choice);
+		return choice;
+	}
+int main(){
+	Automate a;
+	int output;
+	do{
+	output = menu();
+	switch(output){
+		case 1 : {
+			readDot(&a,"demo.dot");
+			printf("lecture du fichier avec succes.\n");
+			break; }
+		case 2 : {
+			automateShow(a); 
+			printf("\n");
+			break;}
+		case 3 : printf("Fin du programme\n"); break;
+		default : printf("Entrer un choix valide s'il vous plait!\n");
+	}
+	}while(output !=3);
+	return 0;
+}
